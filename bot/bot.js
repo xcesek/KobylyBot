d5144cee6b5f02bd2af2a20fbffeeea5f950b07f
Pavol Cesek <paolocesek@gmail.com>
2017-06-30 22:00:15 +0200

    Logging

--- bot/bot.js
+++ bot/bot.js
@@ -7,5 +7,5 @@ var userData = [];
 
 exports.processTextMessage = function (messageText, senderID) {
 
-    sendApi.sendTextMessage(senderID, "Ok, got it. You just came wrote " + messaeText);
+    sendApi.sendTextMessage(senderID, "Ok, got it. You just came wrote " + messageText);
 };
